c4320757556c694a246fe322988647efbec0a160
Omar <omargrard@gmail.com>
2024-12-25 17:40:57 +0800

    chore: switch from tailwind cdn to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
