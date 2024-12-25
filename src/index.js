6e107009fc7876f5c7f5e7815ec947dad84579cf
Omar <omargrard@gmail.com>
2024-12-25 14:29:10 +0800

    feat: added script to always start on top

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-// Initialize Rellax.js for parallax
-const rellax = new Rellax(".parallax");
-
-// Initialize ScrollReveal for animations
-ScrollReveal().reveal("section", {
-  duration: 1000,
-  scale: 0.8,
-  distance: "50px",
-  easing: "ease-in-out",
-  reset: true,
-});
+  
